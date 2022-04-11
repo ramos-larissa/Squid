984b3b1ab808bba938847243ad0c224bcd4ec755
Larissa Ramos <devlarissasr@gmail.com>
2022-04-10 21:23:50 -0300

    add react

---
+++ src/pages/Main/index.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import TestService from "../../service/TestService";
+import CardImage from "../../components/cardImage";
+import logo from "../../assets/logo.svg";
+import "./styles.css";
+
+export default function Main() {
+    const [body, setBody] = useState(0);
+    const url = "/test-front-basic"
+
+    useEffect(() => {
+        TestService.getFront(url)
+            .then((response) => {
+                setBody(response);
+                return body;
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, [url]);
+
+    return (
+        <>
+            <div className="content-title">
+                <img src={logo} className="logo-title" />
+            </div>
+            <div className="content-card">
+                <CardImage response={body} />
+            </div>
+        </>
+    );
+}
